811af473956ddea65b962ce010cea80864d060cc
Alexandre Terebito <terebinto.alexandre@gmail.com>
2022-03-29 23:48:27 -0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,6 +80,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('storage_subdomain') => storage_path('subdomain/app/public'),
     ],
 
 ];
