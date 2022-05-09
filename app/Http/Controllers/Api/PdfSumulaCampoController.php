a2c13df1c32e8d7a47de9cfbbcee9e6f4f2d44b1
Alexandre Terebito <terebinto.alexandre@gmail.com>
2022-05-09 18:07:20 -0300

    Update PdfSumulaCampoController.php

--- app/Http/Controllers/Api/PdfSumulaCampoController.php
+++ app/Http/Controllers/Api/PdfSumulaCampoController.php
@@ -58,12 +58,21 @@ class PdfSumulaCampoController extends Controller
         $createdAt = Carbon::parse($match->m_date);
         $match->m_date = $createdAt->format('d/m/Y');
 
-
-
         $sumula['match'] = $match;
         $sumula['matchday'] = $matchday;
         $sumula['season'] = $season;
         $sumula['tournament'] = $tournament;
+
+        //recuperar cartoes
+        $cartoes = DB::table('nx510_bl_matchday')
+            ->join('nx510_bl_match', 'nx510_bl_matchday.id', '=', 'nx510_bl_match.m_id')
+            ->join('nx510_bl_match_events', 'nx510_bl_match.id', '=', 'nx510_bl_match_events.match_id')
+            ->where('nx510_bl_matchday.s_id', '=', $matchday->s_id)
+            ->where('nx510_bl_match.m_played', '=', '1')
+            ->where('nx510_bl_match_events.e_id', '<', '3')
+            ->orderByRaw('nx510_bl_matchday.id ASC')
+            ->get();
+
 
         //icones
         $sumula['amarelo'] = PdfSumulaCampoController::getiIconeSuspensao($_ENV['SFTP_PATH_ICONES_AMARELO']);
@@ -73,13 +82,51 @@ class PdfSumulaCampoController extends Controller
         $team_1 = Team::find($match->team1_id);
         $team_1->t_initials = substr($team_1->t_name, 0, 3);
 
+
+        $ultimaRodadaTimeA = Matchs::with('matchday')->where('team1_id', '=', $match->team1_id)->orWhere('team2_id', '=', $match->team1_id)->orderBy('nx510_bl_match.m_date', 'DESC')->get();
+
+        $rodadaTime1 = 1;
+        foreach ($ultimaRodadaTimeA  as $ultimaRodada) {
+
+            if ($ultimaRodada->m_played == "1") {
+                if ($ultimaRodada->matchday->s_id == $matchday->s_id) {
+
+                    //  return response()->json($ultimaRodada, 200);
+                    $rodadaTime1 = $ultimaRodada->matchday->m_name;
+                    $team_1['ultimaRodada'] = $ultimaRodada->matchday->m_name;
+                    break;
+                }
+            }
+        }
+
         $players1 = Player::where('team_id', '=', $match->team1_id)->where('position_id', '<', 13)->orderBy('first_name', 'ASC')->get();
         $treinador1 = Player::where('team_id', '=', $match->team1_id)->whereIn('position_id', [12, 16])->orderBy('first_name', 'ASC')->first();
         $auxiliar11 = Player::where('team_id', '=', $match->team1_id)->whereIn('position_id', [13])->orderBy('first_name', 'ASC')->first();
         $auxiliar12 = Player::where('team_id', '=', $match->team1_id)->whereIn('position_id', [14])->orderBy('first_name', 'ASC')->first();
 
+
+        //time 1
         $team_2 = Team::find($match->team2_id);
         $team_2->t_initials = substr($team_2->t_name, 0, 3);
+
+        $ultimaRodadaTimeB = Matchs::with('matchday')->where('team1_id', '=', $match->team2_id)->orWhere('team2_id', '=', $match->team2_id)->orderBy('nx510_bl_match.m_date', 'DESC')->get();
+
+        $rodadaTime2 = 1;
+        foreach ($ultimaRodadaTimeB  as $ultimaRodada) {
+
+            if ($ultimaRodada->m_played == "1") {
+                if ($ultimaRodada->matchday->s_id == $matchday->s_id) {
+
+                    //  return response()->json($ultimaRodada, 200);
+                    $rodadaTime2 = $ultimaRodada->matchday->m_name;
+                    $team_2['ultimaRodada'] = $ultimaRodada->matchday->m_name;
+                    break;
+                }
+            }
+        }
+        //  return response()->json($team_2, 200);
+
+
         $players2 = Player::where('team_id', '=', $match->team2_id)->where('position_id', '<', 13)->orderBy('first_name', 'ASC')->get();
         $treinador2 = Player::where('team_id', '=', $match->team2_id)->whereIn('position_id', [12, 16])->orderBy('first_name', 'ASC')->first();
         $auxiliar21 = Player::where('team_id', '=', $match->team2_id)->whereIn('position_id', [13])->orderBy('first_name', 'ASC')->first();
@@ -110,7 +157,7 @@ class PdfSumulaCampoController extends Controller
             $team_2['auxiliar22'] = $auxiliar22;
         }
 
-        // return response()->json($team_1, 500);
+
 
         $team_1['logo'] = PdfSumulaCampoController::getLogoClube($team_1);
         $team_2['logo'] = PdfSumulaCampoController::getLogoClube($team_2);
@@ -156,6 +203,9 @@ class PdfSumulaCampoController extends Controller
 
         foreach ($players1  as $p) {
 
+            $p['totalCartaoAmarelo'] = 0;
+            $p['totalCartaoVermelho'] = 0;
+
             //lista de suspensao
             if (isset($suspensao)) {
 
@@ -176,6 +226,27 @@ class PdfSumulaCampoController extends Controller
                                 $p['suspensoPunicao'] = 1;
                                 $p['isSuspenso'] = 1;
                             }
+                        }
+                    }
+                }
+            }
+
+            //verificar cartões
+
+            foreach ($cartoes  as $cartao) {
+
+                if ($p->id == $cartao->player_id) {
+
+                    if ($cartao->e_id == "1") {
+                        $p['totalCartaoAmarelo'] = $p['totalCartaoAmarelo'] + 1;
+                        $p['ultima_rodada_amarelo'] = $cartao->m_name;
+                    } else if ($cartao->e_id == "2") {
+                        $p['totalCartaoVermelho'] = $p['totalCartaoVermelho'] + 1;
+
+                        //verificar se a expulsao foi na rodada anterior
+                        if ($cartao->m_name == ($matchday->m_name - 1)) {
+                            $p['isSuspenso'] = 1;
+                            $p['suspensoVermelho'] = 1;
                         }
                     }
                 }
@@ -185,6 +256,9 @@ class PdfSumulaCampoController extends Controller
 
         foreach ($players2  as $p) {
 
+            $p['totalCartaoAmarelo'] = 0;
+            $p['totalCartaoVermelho'] = 0;
+
             //lista de suspensao
             if (isset($suspensao)) {
 
@@ -209,8 +283,53 @@ class PdfSumulaCampoController extends Controller
                     }
                 }
             }
+
+            foreach ($cartoes  as $cartao) {
+
+                if ($p->id == $cartao->player_id) {
+
+                    if ($cartao->e_id == "1") {
+
+                        $p['totalCartaoAmarelo'] = $p['totalCartaoAmarelo'] + 1;
+                        $p['ultima_rodada_amarelo'] = $cartao->m_name;
+                    } else if ($cartao->e_id == "2") {
+                        $p['totalCartaoVermelho'] = $p['totalCartaoVermelho'] + 1;
+
+                        //verificar se a expulsao foi na rodada anterior
+                        if ($cartao->m_name == ($matchday->m_name - 1)) {
+                            $p['isSuspenso'] = 1;
+                            $p['suspensoVermelho'] = 1;
+                        }
+                    }
+                }
+            }
         }
 
+        //verificar cartões - amarelo
+
+        foreach ($players1  as $p) {
+
+            //se o cartao amarelo for multiplo de 3
+            if (($p->totalCartaoAmarelo % 3) == 0) {
+
+                if ($p['ultima_rodada_amarelo'] == ($team_1['ultimaRodada'])) {
+                    $p['isSuspenso'] = 1;
+                    $p['suspensoAmarelo'] = 1;
+                }
+            }
+        }
+
+        foreach ($players2  as $p) {
+
+            //se o cartao amarelo for multiplo de 3
+            if (($p->totalCartaoAmarelo % 3) == 0) {
+
+                if ($p['ultima_rodada_amarelo'] == ($team_2['ultimaRodada'])) {
+                    $p['isSuspenso'] = 1;
+                    $p['suspensoAmarelo'] = 1;
+                }
+            }
+        }
 
         $sumula['team_1'] = $team_1;
         $sumula['team_2'] = $team_2;
@@ -240,12 +359,10 @@ class PdfSumulaCampoController extends Controller
         $sumula['vagasInscricoes'] = $arrayVagasInscricoes1;
         $sumula['vagasInscricoes2'] = $arrayVagasInscricoes2;
 
-
-
         $sumula['totalPartidasTime1'] = $totalJogos1;
         $sumula['totalPartidasTime2'] = $totalJogos2;
 
-        // return response()->json($players2, 500);
+        //return response()->json($players2, 500);
 
         $data = date("FjYg:ia");
         $sumula['logo'] = PdfSumulaCampoController::getLogo($tournament);
